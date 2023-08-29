b7fabf82c56d3811d404ad375ddd9707e0255c08
ROHIT-090 <124237810+ROHIT-090@users.noreply.github.com>
2023-08-29 23:33:28 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/Footer.css";
+import SubscribeNewsletter from "./SubscribeNewsletter";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-section">
+      
+    </div>
+  );
+}
+
+export default Footer;
